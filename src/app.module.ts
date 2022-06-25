efebac242f404716ef46dd17b2ef77a36071b9ef
Marcin Bilka <bilkadev@gmail.com>
2022-06-25 19:28:57 +0200

    Nestjs generate module,controllers and services recipe.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { ListModule } from './list/list.module';
+import { RecipeModule } from './recipe/recipe.module';
 
 @Module({
-  imports: [ProductModule, ListModule],
+  imports: [ProductModule, ListModule, RecipeModule],
   controllers: [AppController],
   providers: [AppService],
 })
