74ea25262cc386f03280de75c0b3e5b30868c377
Marcin Bilka <bilkadev@gmail.com>
2022-06-25 19:27:48 +0200

    Nestjs generate module,controllers and services list.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { ListModule } from './list/list.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, ListModule],
   controllers: [AppController],
   providers: [AppService],
 })
