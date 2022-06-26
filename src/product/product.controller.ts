702eff3d8e674a2a46462ebbca49a54530a147b0
Marcin Bilka <bilkadev@gmail.com>
2022-06-26 07:35:10 +0200

    Added service and controls to product get

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get, Inject, Param} from '@nestjs/common';
+import {ProductService} from "./product.service";
+import {GetProductResponse, ProductListResponse} from "../interfaces/product/product";
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+    constructor(
+        @Inject(ProductService) private productService: ProductService,
+    ) {}
+
+    @Get('/')
+    getProductsList():Promise<ProductListResponse> {
+        return this.productService.getProducts()
+    }
+    @Get('/:id')
+    getProduct(
+        @Param('id') id: string,
+    ):Promise<GetProductResponse> {
+        return this.productService.getProduct(id)
+    }
+}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import {Product} from "./product.entity";
+import {GetProductResponse, ProductListResponse} from "../interfaces/product/product";
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+    async getProducts(): Promise<ProductListResponse> {
+        return await Product.find()
+    }
+
+    async getProduct(id): Promise<GetProductResponse> {
+        return await Product.findOneOrFail({where: {id}})
+    }
+
+    async hasProducts(name: string): Promise<boolean> {
+        return (await this.getProducts()).some(product => product.name === name)
+    }
+
+}
