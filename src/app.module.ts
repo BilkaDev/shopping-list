25fe754fa711415fe668fd2879420ab8a3702b4b
Marcin Biłka <98228475+BilkaDev@users.noreply.github.com>
2022-11-25 14:46:54 +0100

    deleted app controller and service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { ProductModule } from "./product/product.module";
 import { ListModule } from "./list/list.module";
 import { RecipeModule } from "./recipe/recipe.module";
@@ -23,7 +21,5 @@ import { MailModule } from "./mail/mail.module";
     }),
     MailModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
