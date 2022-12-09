514bd167c60398cf158574f9fa574be4fe4f4bad
Marcin Biłka <98228475+BilkaDev@users.noreply.github.com>
2022-12-09 16:05:29 +0100

    Change sing-up template.

--- src/templates/sing-up.ts
+++ src/templates/sing-up.ts
@@ -1,9 +1,13 @@
 export function singUpEmailTemplate() {
   return `
-     <h1>Hello User!</h1>
-     </br>
-     <p>Congratulations. your account has been created</p>
-     </br>
-     <p>Enjoy your day!</p>
+    <h1>Dear User!</h1>
+    </br>
+    <p>Thank you for completing your registration with <a href="https://shopping-list.bilka.networkmanager.pl/">shopping-list.bilka.networkmanager.pl/</a>.</p>
+    </br>
+    <p>This email serves as a confirmation that your account is activated and that you are officially a part of the shopping-list family.
+      Enjoy!</p>
+    <br>
+    <p>Regards,</p>
+    <p>The Shopping List Team!</p>
      `;
 }
